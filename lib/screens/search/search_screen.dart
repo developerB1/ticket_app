0cd24fd4bf3a8edc228fe650cf923049c19940e2
Bircan E. <bircan.emtekin@hotmail.com>
2025-01-17 05:04:27 +0100

    modify search screen and bottom nav bar files

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/core/res/styles/app_styles.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "What are\nyou looking for?",
+            style: AppStyles.headLineStyle1.copyWith(
+              fontSize: 35,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
