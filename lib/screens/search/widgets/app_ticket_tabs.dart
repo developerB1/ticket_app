9d84ddce5cc8ee7b38e263db5ffd9bb31c0d2994
Bircan E. <bircan.emtekin@hotmail.com>
2025-01-22 05:46:25 +0100

    modify app ticket tabs

--- lib/screens/search/widgets/app_ticket_tabs.dart
+++ lib/screens/search/widgets/app_ticket_tabs.dart
@@ -13,33 +13,43 @@ class AppTicketTabs extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 7),
-            width: size.width * .44,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.horizontal(
-                left: Radius.circular(50),
-              ),
-            ),
-            child: Center(
-              child: Text("Airline Tickets"),
-            ),
+          AppTabs(
+            tabText: "All Tickets",
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 7),
-            width: size.width * .44,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.horizontal(
-                right: Radius.circular(50),
-              ),
-            ),
-            child: Center(
-              child: Text("Hotels"),
-            ),
+          AppTabs(
+            tabText: "Hotels",
+            tabBorder: true,
+            tabColor: true,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class AppTabs extends StatelessWidget {
+  const AppTabs(
+      {super.key,
+      this.tabText = "",
+      this.tabBorder = false,
+      this.tabColor = false});
+  final String tabText;
+  final bool tabBorder;
+  final bool tabColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 7),
+      width: size.width * .44,
+      decoration: BoxDecoration(
+          color: tabColor == false ? Colors.white : Colors.transparent,
+          borderRadius: tabBorder == false
+              ? BorderRadius.horizontal(left: Radius.circular(50))
+              : BorderRadius.horizontal(right: Radius.circular(50))),
+      child: Center(
+        child: Text(tabText),
       ),
     );
   }
